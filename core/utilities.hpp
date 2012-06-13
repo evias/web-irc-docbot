582caae4f1f8b8e92f8123d00a8bcd1f21848ee6
Greg Saive <gregory.saive@sedo.com>
2012-06-13 17:00:05 +0200

    dev: implement evias::utilities::map_intersect_key, port vector_join and in_vector to header file for inline declaration

--- core/utilities.hpp
+++ core/utilities.hpp
@@ -13,31 +13,65 @@
 namespace evias {
 namespace utilities {
 
-    std::string vector_join (std::vector<std::string>, std::string = " ");
-    std::string map_to_json (std::map<std::string, std::string>);
+    using std::string;
+    using std::vector;
+    using std::map;
+
+    inline string vector_join (vector<string> input, string delim = " ")
+    {
+        string output = "";
+        vector<string>::const_iterator it = input.begin();
+        for (int i = 0; it != input.end(); it++, i++)
+            output = output + (i > 0 ? delim : "") + (*it);
+
+        return output;
+    }
+
+    inline string map_to_json(map<string,string> input)
+    {
+        string json = "";
+        map<string,string>::const_iterator it = input.begin();
+        for (int i = 0; it != input.end(); it++, i++)
+            json = json + (i == 0 ? "{" : ",") + "'" + (*it).first + "'='" + (*it).second + "'";
+
+        return json;
+    }
 
     template <typename T>
-    inline bool in_vector(T f, std::vector<T> v)
+    inline bool in_vector(T f, vector<T> v)
     {
-        typename std::vector<T>::const_iterator i;
+        typename vector<T>::const_iterator i;
         for (i = v.begin(); i != v.end(); i++)
             if (*i == f)
                 return true;
         return false;
     }
 
-    inline std::string trim(const std::string& s, std::string sz = " \n")
+    inline string trim(const string& s, string sz = " \t\r\n")
     {
-        using std::string;
-
         string out = s;
-        string::size_type pos  = 0;
+        string::size_type pos = 0;
 
-        do out = out.replace(pos, 1, "");
+        do if (pos > 0)
+            out = out.replace(pos, 1, "");
         while ((pos = out.find_last_of(sz)) != string::npos
                && pos == out.size()-1);
 
         return out;
+    }
+
+    template <typename first_t, typename second_t>
+    inline map<first_t, second_t> map_intersect_key(map<first_t, second_t> m1, map<first_t, second_t> m2)
+    {
+        map<first_t, second_t> intersection;
+        typename map<first_t, second_t>::iterator it_keys   = m1.begin();
+        typename map<first_t, second_t>::iterator it_values = m1.begin();
+        for (; it_keys != m1.end(); it_keys++)
+            if ((it_values = m2.find(it_keys->first)) != m2.end())
+                // keys from map1, values from map2
+                intersection.insert(make_pair(it_keys->first, it_values->second));
+
+        return intersection;
     }
 
 };
